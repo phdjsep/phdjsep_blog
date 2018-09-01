7e5dbdae68a862c8490e29b015da4d80185703a7
Sep Dadsetan <sep@syapse.com>
2018-09-01 16:27:24 -0700

    nationals 2018 all rounds

--- scripts/clean_scores.R
+++ scripts/clean_scores.R
@@ -4,36 +4,36 @@ library(jsonlite)
 # Need to prep text output from selenium to be in JSON format before running here
 # This assumes three rounds
 
-raw <- read_json("rdata/footgolf/nationals/nationals_scores_seniors.json", simplifyVector = T)
-raw_unnested <- raw %>% unnest() %>% select(name, round1, round2)
+raw <- read_json("rdata/footgolf/nationals/nationals_scores_men.json", simplifyVector = T)
+raw_unnested <- raw %>% unnest() %>% select(name, round1, round2, round3)
 
 scores_rd1 <- select(raw_unnested, name, round1)
 scores_rd2 <- select(raw_unnested, name, round2)
-# scores_rd3 <- select(raw_unnested, name, round3)
+scores_rd3 <- select(raw_unnested, name, round3)
 
-scores_rd1 <- add_column(scores_rd1, round = rep(1, 306), .before = 'round1')
-scores_rd2 <- add_column(scores_rd2, round = rep(2, 306), .before = 'round2')
-# scores_rd3 <- add_column(scores_rd3, round = rep(3, 1800), .before = 'round3')
+scores_rd1 <- add_column(scores_rd1, round = rep(1, 1260), .before = 'round1')
+scores_rd2 <- add_column(scores_rd2, round = rep(2, 1260), .before = 'round2')
+scores_rd3 <- add_column(scores_rd3, round = rep(3, 1260), .before = 'round3')
 
-scores_rd1 <- add_column(scores_rd1, hole = rep(1:18, 17), .before = 'round1')
-scores_rd2 <- add_column(scores_rd2, hole = rep(1:18, 17), .before = 'round2')
-# scores_rd3 <- add_column(scores_rd3, hole = rep(1:18, 100), .before = 'round3')
+scores_rd1 <- add_column(scores_rd1, hole = rep(1:18, 70), .before = 'round1')
+scores_rd2 <- add_column(scores_rd2, hole = rep(1:18, 70), .before = 'round2')
+scores_rd3 <- add_column(scores_rd3, hole = rep(1:18, 70), .before = 'round3')
 
 names(scores_rd1) <- c("player", "round", "hole", "strokes")
 names(scores_rd2) <- c("player", "round", "hole", "strokes")
-# names(scores_rd3) <- c("player", "round", "hole", "strokes")
+names(scores_rd3) <- c("player", "round", "hole", "strokes")
 
 pars <- c(5, 4, 3, 4, 4, 4, 3, 4, 5, 4, 4, 3, 4, 5, 4, 4, 5, 3)
-# yds <- c(269, 220, 124, 202, 173, 192, 134, 219, 258, 174, 153, 75, 195, 301, 209, 181, 260, 71)
+yds <- c(269, 220, 124, 202, 173, 192, 134, 219, 258, 174, 153, 75, 195, 301, 209, 181, 260, 71)
 
-scores_rd1 <- add_column(scores_rd1, par = rep(pars, 17))
-scores_rd2 <- add_column(scores_rd2, par = rep(pars, 17))
-# scores_rd3 <- add_column(scores_rd3, par = rep(pars, 100))
-scores_rd1 <- add_column(scores_rd1, yds = rep(yds, 71))
-scores_rd2 <- add_column(scores_rd2, yds = rep(yds, 71))
-# scores_rd3 <- add_column(scores_rd3, yds = rep(yds, 100))
+scores_rd1 <- add_column(scores_rd1, par = rep(pars, 70))
+scores_rd2 <- add_column(scores_rd2, par = rep(pars, 70))
+scores_rd3 <- add_column(scores_rd3, par = rep(pars, 70))
+# scores_rd1 <- add_column(scores_rd1, yds = rep(yds, 71))
+# scores_rd2 <- add_column(scores_rd2, yds = rep(yds, 71))
+# scores_rd3 <- add_column(scores_rd3, yds = rep(yds, 71))
 
-nationals_scores <- rbind(scores_rd1, scores_rd2) ##, scores_rd3)
+nationals_scores <- rbind(scores_rd1, scores_rd2, scores_rd3)
 nationals_scores <- mutate(nationals_scores, score = strokes - par)
 
 nationals_scores$round <- as.integer(nationals_scores$round)
